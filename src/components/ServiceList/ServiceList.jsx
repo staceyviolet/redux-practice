b7ea56e4d6125d4e8292c5111f699820251f2412
Anastasia Puzanova <stasya19@gmail.com>
2022-04-20 02:39:22 +0700

    fix

--- src/components/ServiceList/ServiceList.jsx
+++ src/components/ServiceList/ServiceList.jsx
@@ -1,22 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
 import { deleteItem } from "../../rtkstore/listReducer";
 
-export function ServiceList({ handleEdit }) {
+export function ServiceList({ handleEdit, setItem }) {
     const filterStr = useSelector((store) => store.listReducer.currentFilter)
 
     const storeItems = useSelector((store) => {
         if (!filterStr) {
             return store.listReducer.items
         } else {
-            return store.listReducer.items.filter(item => item.name.concat(" ").concat(item.price)
+            return store.listReducer.items.filter(item => item.name.concat(" ")
+                .concat(item.price)
                 .toLowerCase()
                 .includes(filterStr.toLowerCase()))
         }
     })
+
     const dispatch = useDispatch()
 
     const handleRemove = (id) => {
         dispatch(deleteItem(id))
+        handleEdit({name: '', price: ''})
     }
 
     const edit = (item) => {
